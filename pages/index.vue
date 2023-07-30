7b73998a5c1af1b978ea5254b7d328844cc1f580
유광엽(Kwangyeop Yoo)/사내시스템팀/11ST <dbtommy@sk.com>
2023-07-30 23:01:24 +0900

    이미지 테스트

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,12 @@
 
 <template>
   <div>
-    index page 입니다.2
+    서령아 안녕? <br/>
+    난 코딩공룡이라고 해.<br/>
+    <br/>
+    오늘은 재밌는 쇼핑하고 맛있는 삼겹살을 먹어서 아주 행복한 하루였어!<br/>
+    보라카이 여행도 재밌게 다녀오자 &#x2764;<br/> 
+    <img src="~/assets/images/yeopnddeo.jpg" style="width: 500px; height: 700px;" />
   </div>
 </template>
 
